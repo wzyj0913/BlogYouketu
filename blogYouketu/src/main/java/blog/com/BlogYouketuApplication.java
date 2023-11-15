03579551fc9429199ace54b57fd0a7f52da258ee
wzyj0913 <yangjie19890913@gmail.com>
2023-11-15 10:14:00 +0900

    プロジェクト作成

---
+++ blogYouketu/src/main/java/blog/com/BlogYouketuApplication.java
@@ -0,0 +1,13 @@
+package blog.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogYouketuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogYouketuApplication.class, args);
+	}
+
+}
